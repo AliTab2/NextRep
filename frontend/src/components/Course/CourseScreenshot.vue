acbb369044867ad32d19c339086c0c67e526a46b
AliTab2 <alawyaloosh2@gmail.com>
2025-06-16 20:51:58 +0200

    Update: Frontend

--- frontend/src/components/Course/CourseScreenshot.vue
+++ frontend/src/components/Course/CourseScreenshot.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="screenshot">
-    <div class="screenshot-brand">
-      <div class="screenshot-brand__main">
+    <div class="screenshot-header">
+      <div class="screenshot-brand">
         <p>NextRep</p>
       </div>
-      <div class="screenshot-brand__sub">
-        <p>CleverFit-Kurse</p>
-      </div>
-    </div>
-    <div class="screenshot-time">
-      <div class="screenshot-time__day">
+      <div class="screenshot-day">
         <p>{{ day || 'Tag' }}</p>
       </div>
-      <div class="screenshot-time__date">
+      <div class="screenshot-date">
         <p>{{ date || 'Datum' }}</p>
       </div>
     </div>
@@ -31,60 +26,41 @@ export default {
 
 <style scoped>
 .screenshot {
-  background-color: rgb(225, 0, 0);
   height: 100%;
   max-width: 54rem;
   aspect-ratio: 9/16;
   display: flex;
   flex-direction: column;
-  padding: 2rem 0;
-  /* margin: 0 auto; */
+  background-image: url('@/assets/imgs/flyer-bg.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: top;
+}
+
+.screenshot-header {
+  margin-top: 3rem;
+  height: 15rem;
+  width: 100%;
+  padding-left: 3rem;
+  font-size: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-end;
+  font-weight: bold;
+  color: black;
+  line-height: normal;
 }
 
 .screenshot-brand {
-  /* background-color: blue; */
-  color: white;
-  height: 10%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.screenshot-brand__main {
-  font-size: 1.8rem;
-  font-weight: bold;
-}
-
-.screenshot-brand__sub {
-  font-size: 2rem;
-  color: black;
-  font-weight: bold;
-}
-
-.screenshot-time {
-  width: 100%;
-  padding: 1rem 1rem 1rem 0;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
-
-.screenshot-time__date,
-.screenshot-time__day {
-  background-color: white;
-  color: red;
-  font-size: 1.6rem;
-  padding: 1rem;
-  width: fit-content;
-  font-weight: bold;
+  color: #db1200;
+  font-size: 3.5rem;
 }
 
 .screenshot-content {
   width: 100%;
   flex: 1;
   padding: 0 4rem;
-  /* background-color: blue; */
   display: flex;
   flex-direction: row;
   justify-content: center;
